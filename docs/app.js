d3770e788528e10e34e07b2f53478f4dbe4bc877
Elsa (Uyen) Nguyen <tnguyen21@northislandcollege.ca>
2024-03-31 09:11:54 -0700

    Add songs data

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,63 @@
-'use strict';
+"use strict";
+
+const musicHome = [
+  {
+    id: 1,
+    name: "Fight song",
+    author: "Rachel Platten",
+    imgUrl: "assets/image/marshmello.jpg",
+    src: "assets/tracks/fightsong.mp3",
+  },
+  {
+    id: 2,
+    name: "Dernière Danse",
+    author: "Indila",
+    imgUrl: "assets/image/billie.jpg",
+    src: "assets/tracks/indila.mp3",
+  },
+  {
+    id: 3,
+    name: "Speechless",
+    author: "Aladdin",
+    imgUrl: "assets/image/weeknd.jpg",
+    src: "assets/tracks/speechless.mp3",
+  },
+  {
+    id: 4,
+    name: "Shape Of My Heart",
+    author: "Backstreet Boys",
+    imgUrl: "assets/image/love.png",
+    src: "assets/tracks/shapeofmyheart.mp3",
+  },
+];
+
+const queueMusic = [
+  {
+    id: 1,
+    name: "Fight song",
+    author: "Rachel Platten",
+    imgUrl: "assets/image/marshmello.jpg",
+    src: "assets/tracks/fightsong.mp3",
+  },
+  {
+    id: 2,
+    name: "Dernière Danse",
+    author: "Indila",
+    imgUrl: "assets/image/billie.jpg",
+    src: "assets/tracks/indila.mp3",
+  },
+  {
+    id: 3,
+    name: "Speechless",
+    author: "Aladdin",
+    imgUrl: "assets/image/weeknd.jpg",
+    src: "assets/tracks/speechless.mp3",
+  },
+  {
+    id: 4,
+    name: "Shape of my Heart",
+    author: "Backstreet Boys",
+    imgUrl: "assets/image/love.png",
+    src: "assets/tracks/shapeofmyheart.mp3",
+  },
+];
